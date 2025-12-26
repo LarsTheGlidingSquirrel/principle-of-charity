07f00c63adb9179ddd827ba11b6737a90a18ac80
Lars <lars@serlo.org>
2025-12-26 09:29:13 +0100

    only load text-block.svg once

--- components/with-background.js
+++ components/with-background.js
@@ -1,5 +1,31 @@
+let svgPromise = null;
+
+async function getSvg() {
+  if (!svgPromise) {
+    svgPromise = new Promise((resolve, reject) => {
+      console.log("Fetching");
+      fetch("/images/text-block.svg").then((response) => {
+        response.text().then(svgText => {
+          const parser = new DOMParser();
+          const doc = parser.parseFromString(svgText, "image/svg+xml");
+          const svg = doc.documentElement;
+          svg.removeAttribute("width");
+          svg.removeAttribute("height");
+          svg.setAttribute("preserveAspectRatio", "none");
+          svg.style.setProperty("width", "100%");
+          svg.style.setProperty("height", "100%");
+          const firstPathElement = svg.querySelector("path");
+          firstPathElement.style.setProperty("fill", "var(--with-background-color)");
+          resolve(svg)
+        });
+      }).catch(() => reject('Failed'));
+    })
+  }
+  return svgPromise
+}
+
 class WithBackground extends HTMLElement {
-  color = '#f0d999'
+  color = "#f0d999";
 
   constructor() {
     super();
@@ -10,35 +36,27 @@ class WithBackground extends HTMLElement {
   }
 
   static get observedAttributes() {
-    return ['color'];
+    return ["color"];
   }
 
   connectedCallback() {
-    this.addBackground()
+    this.addBackground();
   }
 
   async addBackground() {
-    const backgroundContainer = this.shadowRoot.querySelector('.background')
-    if(!backgroundContainer || !this.color) return 
-    const response = await fetch('/images/text-block.svg');
-    const svgText = await response.text();
-    const parser = new DOMParser();
-    const doc = parser.parseFromString(svgText, 'image/svg+xml');
-    const svg = doc.documentElement;
-    svg.removeAttribute('width')
-    svg.removeAttribute('height')
-    svg.setAttribute('preserveAspectRatio', 'none')
-    svg.style.setProperty('width', '100%')
-    svg.style.setProperty('height', '100%')
-    const firstPathElement = svg.querySelector('path')
-    firstPathElement.style.setProperty('fill', 'var(--with-background-color)')
-    backgroundContainer.style.setProperty("--with-background-color", this.color);
-    backgroundContainer.append(svg);
+    const backgroundContainer = this.shadowRoot.querySelector(".background");
+    if (!backgroundContainer || !this.color) return;
+    const svg = await getSvg()
+    backgroundContainer.style.setProperty(
+      "--with-background-color",
+      this.color
+    );
+    backgroundContainer.append(svg.cloneNode(true));
   }
 
   attributeChangedCallback(name, oldValue, newValue) {
     if (name === "color" && oldValue !== newValue) {
-      this.color = newValue
+      this.color = newValue;
     }
   }
 }
